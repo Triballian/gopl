dbcf40ac8db02f28c965b97da693d3db6f25d370
Triballian <candiapps@gmail.com>
2017-01-24 11:14:41 -0600

    added issues.go

--- ch4/movie/main.go
+++ ch4/movie/main.go
@@ -25,11 +25,19 @@ func main() {
 		//...
 	}
 
-	data, err := json.MarshalIndent(movies, "", "   ")
+	data, _ := json.MarshalIndent(movies, "", "   ")
 
-	if err != nil {
-		log.Fatalf("JSON marshaling failed: %s", err)
+	// for checking if marshal worked
 
+	// if err != nil {
+	// 	log.Fatalf("JSON marshaling failed: %s", err)
+
+	// }
+	// fmt.Printf("%s\n", data)
+
+	var titles []struct{ Title string }
+	if err := json.Unmarshal(data, &titles); err != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", err)
 	}
-	fmt.Printf("%s\n", data)
+	fmt.Println(titles) // "[{Casablanca} {Cool Hand Luke} {Bullit}]"
 }
